d6ed66ce989c2dc032c1924f8374fea0d3ee86c7
Bodruddoza Redoy <134510169+BodruddozaRedoy@users.noreply.github.com>
2025-02-18 00:12:21 +0600

    done

--- src/pages/CardPage.jsx
+++ src/pages/CardPage.jsx
@@ -3,6 +3,7 @@ import Section from '../Components/Section';
 import Card1 from '../Components/Cards/Card1';
 import Card2 from '../Components/Cards/Card2';
 import Card3 from '../Components/Cards/Card3';
+import Card3 from '../Components/Cards/Card4';
 
 const CardPage = () => {
     return (
@@ -10,6 +11,7 @@ const CardPage = () => {
             <Card1/>
             <Card2/>
             <Card3/>
+            <Card4/>
         </Section>
     );
 };
